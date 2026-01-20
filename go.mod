051694adb11ac27bb07c92ec55aa2e54eeb548e8
Xe Iaso <xe@tigrisdata.com>
2026-01-20 12:00:59 -0500

    chore: initial commit of code
    Signed-off-by: Xe Iaso <xe@tigrisdata.com>

---
+++ go.mod
@@ -0,0 +1,33 @@
+module github.com/tigrisdata-community/glue
+
+go 1.25.5
+
+tool (
+	github.com/a-h/templ/cmd/templ
+	golang.org/x/tools/cmd/goimports
+	golang.org/x/tools/cmd/stringer
+	honnef.co/go/tools/cmd/staticcheck
+)
+
+require (
+	github.com/BurntSushi/toml v1.4.1-0.20240526193622-a339e1f7089c // indirect
+	github.com/a-h/parse v0.0.0-20250122154542-74294addb73e // indirect
+	github.com/a-h/templ v0.3.977 // indirect
+	github.com/andybalholm/brotli v1.1.0 // indirect
+	github.com/cenkalti/backoff/v4 v4.3.0 // indirect
+	github.com/cli/browser v1.3.0 // indirect
+	github.com/fatih/color v1.16.0 // indirect
+	github.com/fsnotify/fsnotify v1.7.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/natefinch/atomic v1.0.1 // indirect
+	golang.org/x/exp/typeparams v0.0.0-20231108232855-2478ac86f678 // indirect
+	golang.org/x/mod v0.32.0 // indirect
+	golang.org/x/net v0.49.0 // indirect
+	golang.org/x/sync v0.19.0 // indirect
+	golang.org/x/sys v0.40.0 // indirect
+	golang.org/x/telemetry v0.0.0-20260109210033-bd525da824e2 // indirect
+	golang.org/x/tools v0.41.0 // indirect
+	golang.org/x/tools/go/expect v0.1.1-deprecated // indirect
+	honnef.co/go/tools v0.6.1 // indirect
+)
---
+++ web/discordwebhook/webhook.go
@@ -0,0 +1,78 @@
+// Package discordwebhook is a simple low-level HTTP client wrapper around Discord webhooks.
+package discordwebhook
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+
+	"github.com/tigrisdata-community/glue/web"
+)
+
+// Webhook is the parent structure fired off to Discord.
+type Webhook struct {
+	Content         string              `json:"content,omitempty"`
+	Username        string              `json:"username,omitempty"`
+	AvatarURL       string              `json:"avatar_url"`
+	Embeds          []Embeds            `json:"embeds,omitempty"`
+	AllowedMentions map[string][]string `json:"allowed_mentions"`
+}
+
+// EmbedField is an individual field being embedded in a message.
+type EmbedField struct {
+	Name   string `json:"name"`
+	Value  string `json:"value"`
+	Inline bool   `json:"inline"`
+}
+
+// EmbedFooter is the message footer.
+type EmbedFooter struct {
+	Text    string `json:"text"`
+	IconURL string `json:"icon_url"`
+}
+
+// Embeds is a set of embed fields and a footer.
+type Embeds struct {
+	Title       string       `json:"title,omitempty"`
+	Description string       `json:"description,omitempty"`
+	URL         string       `json:"url,omitempty"`
+	Author      *EmbedAuthor `json:"author,omitempty"`
+	Fields      []EmbedField `json:"fields,omitempty"`
+	Footer      *EmbedFooter `json:"footer,omitempty"`
+}
+
+type EmbedAuthor struct {
+	Name    string `json:"name"`
+	URL     string `json:"url"`
+	IconURL string `json:"icon_url"`
+}
+
+// Send returns a request for a Discord webhook.
+func Send(whurl string, w Webhook) *http.Request {
+	if len(w.Username) > 32 {
+		w.Username = w.Username[:32]
+	}
+
+	data, err := json.Marshal(&w)
+	if err != nil {
+		panic(err)
+	}
+
+	req, err := http.NewRequest(http.MethodPost, whurl, bytes.NewBuffer(data))
+	if err != nil {
+		panic(err)
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+
+	return req
+}
+
+// Validate validates the response from Discord.
+func Validate(resp *http.Response) error {
+	if resp.StatusCode != http.StatusNoContent {
+		return web.NewError(http.StatusNoContent, resp)
+	}
+
+	return nil
+}
